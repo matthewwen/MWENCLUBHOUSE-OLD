740e8d2159f68d890f439fcbebb28a4028f3ad64
Matthew Wen <mattwen2018@gmail.com>
2019-12-22 07:23:24 -0600

    add include statements in main

--- ddns/main.c
+++ ddns/main.c
@@ -4,9 +4,13 @@
 #include <inttypes.h>
 #include <getdns/getdns_extra.h>
 
+#include <getdns/getdns_ext_libevent.h>
+#include <getdns/getdns_ext_libev.h>
+#include <getdns/getdns_ext_libuv.h>
+#include <getdns/getdns.h>
+
 int main(int argc, char* argv[]) {
 	getdns_return_t r;
-
 
 	getdns_context * ctxt = NULL;
 	r = getdns_context_create(&ctxt, 1);
