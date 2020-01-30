2934e0c3f1de0f39a4a70a3ca7d380d95ce09cd5
matthewwen <mattwen2018@gmail.com>
2020-01-30 15:55:12 -0600

    making changes with libraries

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char* argv[]) {
+
+    return EXIT_SUCCESS;
+}
+/* vim: set tabstop=4 shiftwidth=4 fileencoding=utf-8 noexpandtab: */
