60cd231c4a8d162a81d869cce3921e436a040443
Matthew Wen <mattwen2018@gmail.com>
2020-02-10 18:29:23 -0600

    less broken

--- src/apihandler.c
+++ src/apihandler.c
@@ -33,11 +33,11 @@ static const char * TEMPLATE_MAINJS = "/main.js";
 static const char * TEMPLATE_CSS    = "/main.css";
 #define MAX_FILE_LENGTH  11
 
-#define FREE(var) do {\
-	if (var != NULL) {\
-		free(var);\
-		var = NULL;\
-	}
+#define FREE(VAR) do {\
+	if (VAR != NULL) {\
+		free(VAR);\
+		VAR = NULL;\
+	}\
 }while(false);
 
 void createPROJ_handler(struct lws * wsi, bool * found, struct request * r) {
@@ -48,41 +48,62 @@ void createPROJ_handler(struct lws * wsi, bool * found, struct request * r) {
 		return;
 	}
 
-	char * pkg_name  = get_custom_header_data(wsi, "pkg-name:");
+	/*char * pkg_name  = get_custom_header_data(wsi, "pkg-name:");
 	char * grid_name = get_custom_header_data(wsi, "grid-name:");
 	char * date_str  = get_custom_header_data(wsi, "mwen-date:");
 
 	bool is_valid = (pkg_name != NULL) || (grid_name != NULL) || (date_str != NULL);
 
+	char * database_name = NULL;
+	if (is_valid == true) {
+		int num_char  = strlen(pkg_name) + strlen(grid_name) + strlen(date_str) + 1;
+		database_name = malloc(sizeof(*database_name) * num_char);
+		is_valid = database_name != NULL;
+	}
+
+	if (is_valid == true) {
+		sprintf(database_name, "%s%s%s", pkg_name, grid_name, date_str);
+	}
+	FREE(pkg_name);
+	FREE(grid_name);
+	FREE(date_str);
+	
 	if (is_valid == false) {
-		FREE(pkg_name);
-		FREE(grid_name);
-		FREE(date_str);
+		free_json(&robj);
+		FREE(database_name)
+		return;
 	}
 
 	char tokenBuffer[65];
-	sprintf(database_name, "%s%s%s", pkg_name, grid_name, date_str);
 	memset(tokenBuffer, 0, sizeof(tokenBuffer));
 	sha256_string(database_name, tokenBuffer);
+	FREE(database_name);
 	tokenBuffer[16] = '\0';
-	size_t alloc_size = sizeof(*locat) * (strlen(TEMPLATE_PARENT) + 16 + MAX_FILE_LENGTH);
-	locat  = malloc(alloc_size);
 
-	memset(locat, 0, alloc_size);
-	sprintf(locat, "%s%s", TEMPLATE_PARENT, tokenBuffer);
-	mkdir(locat, S_IRWXU);
-	sprintf(locat, "%s%s%s", TEMPLATE_PARENT, tokenBuffer, TEMPLATE_MAINJS);
-	FILE * fp = fopen(locat, "w");
+	size_t alloc_size = sizeof(char) * (strlen(TEMPLATE_PARENT) + 16 + MAX_FILE_LENGTH);
+	char * directory  = malloc(alloc_size);
+	if (directory == false) {
+		free_json(&robj);
+		return;
+	}
+
+	memset(directory, 0, alloc_size);
+	sprintf(directory, "%s%s", TEMPLATE_PARENT, tokenBuffer);
+	mkdir(directory, S_IRWXU);
+
+	sprintf(directory, "%s%s%s", TEMPLATE_PARENT, tokenBuffer, TEMPLATE_MAINJS);
+	FILE * fp = fopen(directory, "w");
 	if (fp != NULL) {
 		fclose(fp);
 	}
-	sprintf(locat, "%s%s%s", TEMPLATE_PARENT, tokenBuffer, TEMPLATE_CSS);
-	fp = fopen(locat, "w");
+
+	sprintf(directory, "%s%s%s", TEMPLATE_PARENT, tokenBuffer, TEMPLATE_CSS);
+	fp = fopen(directory, "w");
 	if (fp != NULL) {
 		fclose(fp);
 	}
-	sprintf(locat, "%s%s%s", TEMPLATE_PARENT, tokenBuffer, TEMPLATE_INDEX);
-	fp = fopen(locat, "w");
+	sprintf(directory, "%s%s%s", TEMPLATE_PARENT, tokenBuffer, TEMPLATE_INDEX);
+	fp = fopen(directory, "w");
 	if (fp != NULL) {
 		FILE * fpOld = fopen("www/html/template/template.html", "r");
 		if (fpOld != NULL) {
@@ -95,17 +116,7 @@ void createPROJ_handler(struct lws * wsi, bool * found, struct request * r) {
 			fclose(fpOld);
 		}
 		fclose(fp);
-	}
-
-	int num_char  = strlen(pkg_name) + strlen(grid_name) + strlen(date_str) + 1;
-	database_name = malloc(sizeof(*database_name) * num_char);
-	char * str_address[] = {pkg_name, grid_name, date_str, database_name, locat};
-
-	for (int i = 0; i < 5; i++) {
-		if (str_address[i] != NULL) {
-			free(str_address[i]);
-		}
-	}
+	}*/
 
 	json_tostring(&r->buff, robj, &r->alloc_size);
 	free_json(&robj);
