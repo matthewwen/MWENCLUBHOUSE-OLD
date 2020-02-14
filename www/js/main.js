516a98d4ec28dfe09ea61e04f1e38ce1044dd0d2
Matthew Wen <mattwen2018@gmail.com>
2020-02-14 16:57:15 -0600

    edit the backend to work better' '

--- www/js/main.js
+++ www/js/main.js
@@ -3,7 +3,6 @@ const cjson = Cookies.get();
 const urlParams = new URLSearchParams(window.location.search);
 var section_open = Number.isNaN(parseInt(cjson.section_open)) ? 0 : parseInt(cjson.section_open);
 section_open = urlParams.get('section') == null ? section_open : urlParams.get('section') % 5;
-var password = Cookies.get('mwen_password');
 
 Cookies.set("mwen_pkgName", "main");
 
@@ -12,85 +11,6 @@ var section_url = ['/www/html/home.html', '/www/html/work.html', '/www/html/scho
 var heading_id = ['#home-menu', '#workexp-menu', '#school-menu', '#project-menu', '#about-menu'];
 var title_name = ['Home', 'Work', 'School', 'Projects', 'About']
 var section_html = [null, null, null, null, null];
-var edit_keyword = [109, 119, 101, 110];
-var key_start = 0;
-var create_link = null;
-var can_edit = false;
-
-function resizeGridItem(item) {
-	var grid = document.getElementsByClassName("mwen-grid")[0];
-	var rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
-	var rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
-	var rowSpan = Math.ceil((item.querySelector('.content').getBoundingClientRect().height + rowGap) / (rowHeight + rowGap) + 1);
-	item.style.gridRowEnd = "span " + rowSpan;
-}
-
-function resizeAllGridItems() {
-	var allItems = document.getElementsByClassName("item");
-	for (var x = 0; x < allItems.length; x++) {
-		resizeGridItem(allItems[x]);
-	}
-}
-
-function add_create_link(password) {
-	if (password == null) {
-		return;
-	}
-	if (can_edit == false) {
-		$('.addItem').remove();
-		$.ajax({
-			type: 'GET',
-			headers: {
-				'authorization': $.sha256(JSON.stringify({ 'time': getDate(), 'password': password })),
-				'mwen-date': getDate()
-			},
-			url: '/webedit',
-			contentType: "application/json",
-			success: function (result) {
-				var j = JSON.parse(result);
-				can_edit = j.canEdit;
-				if (can_edit) {
-					add_create_link(password);
-					can_edit = false;
-				}
-			},
-		});
-	}
-	else {
-		if (create_link == null) {
-			$.ajax({
-				type: 'GET',
-				url: 'www/html/template/add.html',
-				contentType: "text/html",
-				success: function (result) {
-					create_link = result;
-					if (create_link != null) {
-						add_create_link(password);
-					}
-				}
-			});
-		}
-		else {
-			var allItems = document.getElementsByClassName("mwen-grid");
-			for (var x = 0; x < allItems.length; x++) {
-				// create div element
-				var temp = document.createElement('div');
-				temp.innerHTML = create_link;
-				temp = temp.firstChild;
-
-				// put element inside grid
-				allItems[x].prepend(temp);
-				resizeAllGridItems();
-
-				// activate button
-				temp = temp.getElementsByClassName("mdc-fab")[0];
-				mdc.ripple.MDCRipple.attachTo(temp);
-				temp.href = "createpkg?grid=" + allItems[x].getAttribute('id') + '&name=main';
-			}
-		}
-	}
-}
-
 
 function init() {
 	load_section(section_open);
@@ -124,47 +44,10 @@ function init() {
 function load_body(html, new_selected) {
 	$('#div-body').html(html);
 	section_open = new_selected;
-	add_create_link(password);
+	var password = Cookies.get('mwen_password');
+	add_create_page(password);
 	resizeAllGridItems();
-
-	function add_content_grid() {
-		$('.dynamic').remove();
-		var allItems = document.getElementsByClassName("mwen-grid");
-		for (var x = 0; x < allItems.length; x++) {
-			var gridName = allItems[x].getAttribute('id');
-			$.ajax({type: 'GET',
-				url: '/mwenGetPROJ?pkg-name='+ref+'&grid-name='+gridName,
-				contentType: 'plain/text',
-				success: function(result){
-					var json = JSON.parse(result);
-					if (json == null || json.data == null)  {
-						return;
-					}
-					if (json.gridName == null) {
-						return;
-					}
-					var gridName = json.gridName;
-					for (var i = 0; i < json.data.length; i++) {
-
-						// create div element
-						var content = document.createElement('div');
-						content.setAttribute('class', 'content');
-						content.innerHTML = json.data[i].body;
-
-						var item = document.createElement('a');
-						item.setAttribute('class', 'item dynamic');
-						item.setAttribute('href', 'pageview?name=' + json.data[i].ref );
-						item.appendChild(content);
-
-						// put element inside grid
-						document.getElementById(gridName).append(item);
-						resizeAllGridItems();
-					}
-				}});
-		}
-	}
-
-	add_content_grid();
+	add_content_gpage();
 }
 
 function load_section(new_selected) {
@@ -185,39 +68,24 @@ function load_section(new_selected) {
 	}
 }
 
-function getDate() {
-	var London = new Date().toLocaleString("en-US", { timeZone: "Europe/London" });
-	var now = new Date(London);
-	var tzo = (now.getTimezoneOffset());
-	var pad = function (num) {
-		var norm = Math.abs(Math.floor(num));
-		return (norm < 10 ? '0' : '') + norm;
-	};
-	return pad(now.getMonth() + 1) +
-		pad(now.getDate()) +
-		now.getFullYear() + 'T' +
-		pad(now.getHours()) +
-		pad(now.getMinutes()) +
-		pad(now.getSeconds());
-}
+window.onload = init;
 
+// all webpages should check if they have admin privalges
+var MWENKEYSTART = 0;
 $(document).keypress(function (event) {
-	if (event.charCode == edit_keyword[key_start]) {
-		key_start++;
+	console.log('key press: ' + MWENKEYSTART + ", char code: " + event.charCode);
+	var edit_keyword = [109, 119, 101, 110];
+	if (event.charCode == edit_keyword[MWENKEYSTART]) {
+		MWENKEYSTART++;
 	}
 	else {
-		key_start = 0;
+		MWENKEYSTART = 0;
 	}
 
-	if (key_start == 4) {
-		key_start = 0;
-		password = prompt("Hey Vikram. What's up", "password");
+	if (MWENKEYSTART == 4) {
+		MWENKEYSTART = 0;
+		var password = prompt("Hey Vikram. What's up", "password");
 		Cookies.set("mwen_password", password);
-		add_create_link(password);
+		add_create_page(password);
 	}
 });
-
-window.onload = init;
-window.onresize = function () {
-	resizeAllGridItems();
-};
--- www/js/mwen.js
+++ www/js/mwen.js
@@ -1,13 +1,144 @@
-const header = document.querySelector('.main-header');
+const MWENHEADER = document.querySelector('.main-header');
 
+// for the header
 window.addEventListener('scroll', () => {
 	const scrollPos = window.scrollY;
-	if (header != null) {
+	if (MWENHEADER != null) {
 		if (scrollPos > 10) {
-			header.classList.add('scrolled');
+			MWENHEADER.classList.add('scrolled');
 		}
 		else {
-			header.classList.remove('scrolled');
+			MWENHEADER.classList.remove('scrolled');
 		}
 	}
 });
+
+// for any grid on the page
+function resizeAllGridItems() {
+	var allItems = document.getElementsByClassName("item");
+	for (var x = 0; x < allItems.length; x++) {
+		var item = allItems[x];
+		var grid = document.getElementsByClassName("mwen-grid")[0];
+		var rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
+		var rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
+		var rowSpan = Math.ceil((item.querySelector('.content').getBoundingClientRect().height + rowGap) / (rowHeight + rowGap) + 1);
+		item.style.gridRowEnd = "span " + rowSpan;
+	}
+}
+window.addEventListener('resize', resizeAllGridItems);
+
+// get date for tokenization
+function getDate() {
+	var London = new Date().toLocaleString("en-US", { timeZone: "Europe/London" });
+	var now = new Date(London);
+	var pad = function (num) {
+		var norm = Math.abs(Math.floor(num));
+		return (norm < 10 ? '0' : '') + norm;
+	};
+	return pad(now.getMonth() + 1) + pad(now.getDate()) + now.getFullYear() + 'T' + pad(now.getHours()) + pad(now.getMinutes()) + pad(now.getSeconds());
+}
+
+// on the grids, add the create link page
+var MWENTEMPLATECREATEPAGE = null;
+function add_create_page(userPassword) {
+	add_create_page_helper(userPassword, false);
+	function add_create_page_helper(userPasswordHelper, userCanEditHelper) {
+		if (userPasswordHelper == null) {
+			return;
+		}
+		if (userCanEditHelper == false) {
+			$('.addItem').remove();
+			$.ajax({
+				type: 'GET',
+				headers: {
+					'authorization': $.sha256(JSON.stringify({ 'time': getDate(), 'password': userPassword })),
+					'mwen-date': getDate()
+				},
+				url: '/webedit',
+				contentType: "application/json",
+				success: function (result) {
+					var j = JSON.parse(result);
+					if (j.canEdit) {
+						add_create_page_helper(userPasswordHelper, true);
+					}
+				},
+			});
+		}
+		else {
+			if (MWENTEMPLATECREATEPAGE == null) {
+				$.ajax({
+					type: 'GET',
+					url: 'www/html/template/add.html',
+					contentType: "text/html",
+					success: function (result) {
+						MWENTEMPLATECREATEPAGE = result;
+						if (MWENTEMPLATECREATEPAGE != null) {
+							add_create_page_helper(userPasswordHelper, true);
+						}
+					}
+				});
+			}
+			else {
+				var allItems = document.getElementsByClassName("mwen-grid");
+				for (var x = 0; x < allItems.length; x++) {
+					// create div element
+					var temp = document.createElement('div');
+					temp.innerHTML = MWENTEMPLATECREATEPAGE;
+					temp = temp.firstChild;
+
+					// put element inside grid
+					allItems[x].prepend(temp);
+					resizeAllGridItems();
+
+					// activate button
+					temp = temp.getElementsByClassName("mdc-fab")[0];
+					mdc.ripple.MDCRipple.attachTo(temp);
+					temp.href = "createpkg?grid=" + allItems[x].getAttribute('id') + '&name=main';
+				}
+			}
+		}
+	}
+}
+
+// add grid to reference to different web pages
+function add_content_gpage() {
+	if (!window.hasOwnProperty("ref")) {
+		return;
+	}
+	$('.dynamic').remove();
+	var allItems = document.getElementsByClassName("mwen-grid");
+	for (var x = 0; x < allItems.length; x++) {
+		var gridName = allItems[x].getAttribute('id');
+		$.ajax({
+			type: 'GET',
+			url: '/mwenGetPROJ?pkg-name=' + ref + '&grid-name=' + gridName,
+			contentType: 'plain/text',
+			success: function (result) {
+				var json = JSON.parse(result);
+				if (json == null || json.data == null) {
+					return;
+				}
+				if (json.gridName == null) {
+					return;
+				}
+				var gridName = json.gridName;
+				for (var i = 0; i < json.data.length; i++) {
+
+					// create div element
+					var content = document.createElement('div');
+					content.setAttribute('class', 'content');
+					content.innerHTML = json.data[i].body;
+
+					var item = document.createElement('a');
+					item.setAttribute('class', 'item dynamic');
+					item.setAttribute('href', 'pageview?name=' + json.data[i].ref);
+					item.appendChild(content);
+
+					// put element inside grid
+					document.getElementById(gridName).append(item);
+					resizeAllGridItems();
+				}
+			}
+		});
+	}
+}
