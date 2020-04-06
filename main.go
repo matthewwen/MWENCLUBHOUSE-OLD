c2400384a3d38a1a501e2f61ed7858f7d2155761
Matthew Wen <mattwen2018@gmail.com>
2020-04-06 13:40:31 -0500

    attempt 1 to add tls

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	_ "fmt"
+	"fmt"
 	"net/http"
 	"time"
 	_ "strings"
-	_ "os"
+	"os"
 	_ "io"
+	str "strings"
 	//"strconv"
 
 	// goczmq "gopkg.in/zeromq/goczmq.v4"
@@ -16,12 +17,30 @@ import (
 func main() {
 	var h gosrc.Handler;
 
-	s := &http.Server{
-		Addr:           ":80",
-		Handler:        h,
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
-		MaxHeaderBytes: 1 << 20,
+	argsWithProg := os.Args
+	var dev bool = true 
+	if (len(argsWithProg) == 2) {
+		dev = str.Compare(argsWithProg[1], "dep") != 0
 	}
-	s.ListenAndServe()
+
+	var s &http.Server;
+	if (dev) {
+		s = &http.Server{
+			Addr:           ":80",
+			Handler:        h,
+			ReadTimeout:    10 * time.Second,
+			WriteTimeout:   10 * time.Second,
+			MaxHeaderBytes: 1 << 20,
+		}
+		s.ListenAndServe()
+	} else {
+		s = &http.Server{
+			Addr:           ":443",
+			Handler:        h,
+			ReadTimeout:    10 * time.Second,
+			WriteTimeout:   10 * time.Second,
+			MaxHeaderBytes: 1 << 20,
+		}
+		s.ListenAndServeTLS("/etc/letsencrypt/live/www.matthewwen.com/fullchain.pem", "/etc/letsencrypt/live/www.matthewwen.com/privkey.pem")
+	}
 }
