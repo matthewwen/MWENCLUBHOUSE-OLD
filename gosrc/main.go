f7f918a2cf6f53c0ee083ea7c0a4b1c5c7164576
Matthew Wen <mattwen2018@gmail.com>
2020-04-07 11:13:05 -0500

    remove gethandler

--- gosrc/main.go
+++ gosrc/main.go
@@ -92,3 +92,13 @@ func send_file(path string, found * bool, w http.ResponseWriter, r * http.Reques
 	io.Copy(w, file) 
 	return 0
 }
+
+func (h * Handler) handle_get_request(w http.ResponseWriter, r * http.Request) {
+	var found bool = false
+	h.handle_gweb_request(&found, w, r)
+	h.handle_gapi_request(&found, w, r)
+
+	if (!found) {
+		http.Error(w, "Request Not Found", 404)
+	}
+}
